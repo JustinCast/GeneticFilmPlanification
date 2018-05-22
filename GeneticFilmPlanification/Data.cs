055a1cab664a5da9cd1da3c2d3663351f7963958
Unknown <luisgoncal98@gmail.com>
2018-05-21 23:54:21 -0600

    Metodos de crear escenas, escenarios, localización y actores en la clase Data

--- GeneticFilmPlanification/Data.cs
+++ GeneticFilmPlanification/Data.cs
@@ -9,18 +9,46 @@ namespace GeneticFilmPlanification
 {
     class Data
     {
-        List<Actor> Actors = new List<Actor>();
+        static List<FilmingCalendar> Scenarios = new List<FilmingCalendar>();
+        static List<Actor> Actors = new List<Actor>();
+        static List<Location> Locations = new List<Location>();
 
-        public static void createActors(int numberOfActors) {
-            for (int i=0; i<=numberOfActors;i++) {
+        public static void createActors(int numberOfActors)
+        {
+            for (int i = 0; i <= numberOfActors; i++)
+            {
                 Actor newActor = new Actor();
-                newActor.
-                
-                
-
+                newActor.CostPerDay = 0;
+                newActor.ID = i.ToString();
+                Actors.Add(newActor);
             }
+        }
 
+        public static void createLocation(int numberOfLocations)
+        {
+            for (int i = 0; i <= numberOfLocations; i++)
+            {
+                Location newLocation = new Location();
+                newLocation.ID = i.ToString();
+                newLocation.InUse = false;
+                Locations.Add(newLocation);
+            }
+        }
+        public static void createScenes(int numberOfScenes,FilmingCalendar calendar) {
+            int pages = 5;
+            for (int i = 0; i <= numberOfScenes; i++) {
+                Scene newScene = new Scene();
+                newScene.Pages = pages+1;
+                newScene.Duration = pages * 5;// cada pagina tiene un valor de 5 minutos
+                
+                calendar.Scenes.Add(newScene);
+            }
+        }
 
+        public static void createScenario(int numberOfLocation, int numberOfActors, int numberOfScenes, FilmingCalendar calendar) {
+            createLocation(numberOfLocation);
+            createActors(numberOfActors);
+            createScenes(numberOfScenes,calendar);
         }
     }
 }
