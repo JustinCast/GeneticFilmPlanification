fec94d2bf4d33c52e969b9e72c609e264bdccaed
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-23 21:56:02 -0600

    Clase para calcular el costo

---
+++ GeneticFilmPlanification/ProblemSolver.cs
@@ -0,0 +1,27 @@
+﻿using GeneticFilmPlanification.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticFilmPlanification
+{
+    class ProblemSolver
+    {
+        private class MovieOptimizationSolution
+        {
+            public List<FilmingCalendar> Calendars;
+
+            MovieOptimizationSolution(List<FilmingCalendar> calendars)
+            {
+                this.Calendars = calendars;
+            }
+            
+            public double CalendarCost()
+            {
+                return 0.0;
+            }
+        }
+    }
+}
