654e1d043dda2ce7627d660b86de801cd0e990b9
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-23 23:02:43 -0600

    Método para Obtener el costo de cada actor

--- GeneticFilmPlanification/Models/FilmingCalendar.cs
+++ GeneticFilmPlanification/Models/FilmingCalendar.cs
@@ -10,5 +10,20 @@ namespace GeneticFilmPlanification.Models
     {
         public List<Scene> Scenes = new List<Scene>();
         public int Cost { get; set; }
+
+
+        /// <summary>
+        /// Calcula el costo total del calendario
+        /// </summary>
+        /// <returns>Costo total del calendario</returns>
+        public int CalendarCost()
+        {
+            this.Cost = 0;
+            foreach(Scene s in Scenes)
+            {
+                Cost += s.Act
+            }
+            return 0;
+        }
     }
 }
--- GeneticFilmPlanification/Models/Movie.cs
+++ GeneticFilmPlanification/Models/Movie.cs
@@ -11,6 +11,17 @@ namespace GeneticFilmPlanification.Models
         public List<Actor> Actors { get; set; }   
         public List<Location> Locations { get; set; }
         public List<FilmingCalendar> FilmingCalendars { get; set; }
+
+
+        public int GetActorCost(String id)
+        {
+            foreach(Actor a in Actors)
+            {
+                if (a.Equals(id))
+                    return a.CostPerDay;
+            }
+            return 0;
+        }
     }
 }
 
