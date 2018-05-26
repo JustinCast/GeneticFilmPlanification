ec7239b79a59e39c8b9126a45dbdf0a533fc2cad
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-25 19:26:47 -0600

    Cambios minimos

--- GeneticFilmPlanification/Models/FilmingCalendar.cs
+++ GeneticFilmPlanification/Models/FilmingCalendar.cs
@@ -21,7 +21,7 @@ namespace GeneticFilmPlanification.Models
             this.Cost = 0;
             foreach(Scene s in Scenes)
             {
-                Cost += 
+                //Cost += 
             }
             return 0;
         }
