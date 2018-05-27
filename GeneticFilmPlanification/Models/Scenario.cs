788b30e78642808906bcc7a7e215177df393779f
Unknown <luisgoncal98@gmail.com>
2018-05-27 11:26:10 -0600

    Creacion de metodos en PMX createListWithDays, availableSpace,   canAssignedToActor y accommodateScenesInDays

--- GeneticFilmPlanification/Models/Scenario.cs
+++ GeneticFilmPlanification/Models/Scenario.cs
@@ -13,5 +13,6 @@ namespace GeneticFilmPlanification.Models
         public List<Actor> Actors = new List<Actor>();
         public List<Location> Locations = new List<Location>();
         public List<Day> Days = new List<Day>();
+        public List<List<Day>> possibleDays = new List<List<Day>>();// PMX = se ingresaran listas con los posibles dias en diferentes ordenes en el que se realizaran las escenas
     }
 }
--- GeneticFilmPlanification/Pmx.cs
+++ GeneticFilmPlanification/Pmx.cs
@@ -9,9 +9,9 @@ namespace GeneticFilmPlanification
 {
     class Pmx
     {
-        public static FilmingCalendar generateChromosome(FilmingCalendar calendar) {// se altera el orden de calendario para crear otro pero en orden distinto
+        public static FilmingCalendar generateChromosome( FilmingCalendar calendar) {// Se recorre las escenas de un calendario y se le van agregando inversamente a otro
             FilmingCalendar currentCalendar = calendar;
-            FilmingCalendar newCalendar= new FilmingCalendar();
+            FilmingCalendar newCalendar = new FilmingCalendar();
             for (int i =currentCalendar.Scenes.Count-1;i>=0;i--) {
                 newCalendar.Scenes.Add(currentCalendar.Scenes[i]);
             }
@@ -37,11 +37,11 @@ namespace GeneticFilmPlanification
             return sonCalendar;
         }
 
-        public static void performCrossingPMX(FilmingCalendar calendar,Movie movie, int positionScenario) {// se realiza el cruce entre los dos calendarios para obtener otro mediante un cruce
+        public static void performCrossingPMX(FilmingCalendar calendar,Movie movie, int positionScenario) {// Se realiza el cruce de las escenas 
             FilmingCalendar chromosome1;                                    // Recalcar este metodo crea dos descendientes a la vez
             FilmingCalendar chromosome2;
-            chromosome1 = calendar;
-            chromosome2 = generateChromosome(calendar);
+            chromosome1 = movie.Scenarios[positionScenario].FilmingCalendars[0];
+            chromosome2 = generateChromosome(chromosome1);
             int size = calendar.Scenes.Count;
             int start = (size - size / 2) / 2;
             int end = size - start;
@@ -81,5 +81,97 @@ namespace GeneticFilmPlanification
             }
             return bestCalendar;
         }
+
+        public static List<Day> createListWithDays(FilmingCalendar calendar) {
+            List<Day> newDays = new List<Day>();
+            for (int i = 1; i <= calendar.Scenes.Count; i++)// se crean los dias para asignarle las escenas que ya fueron cruzdas  
+            {
+                Day newDay = new Day();
+                newDay.DayNumber = i;
+                Time time1 = new Time();
+                time1.MaximunScriptPages = 45;
+                Time time2 = new Time();
+                time2.MaximunScriptPages = 45;
+                newDay.DayTime = time1;
+                newDay.NightTime = time2;
+                newDays.Add(newDay);
+            }
+            return newDays;
+        }
+
+        public static int availableSpace(Time Shedule) {// retorna cuanto espacio tiene la jornada segun sus paginas 
+            int space=0;
+            foreach (Scene scene in Shedule.Scenes) {
+                space += scene.Pages;
+            }
+            return Shedule.MaximunScriptPages - space;// Se le resta el espacio tatal al que ya posee, para obtener el que esta disponible 
+        }
+
+        public static bool canAssignedToActor(Day currentDay, List<Day> newDays, Scene scene) {// verifica el respeto de las horas a lavorar de cada actor 
+            Day before;
+            if (newDays.IndexOf(currentDay) != 0  ) {//contexto de jornada de dia 
+                before = newDays.ElementAt(newDays.IndexOf(currentDay) - 1);// el dia anterior 
+                if (scene.Schedule == true) {  // si la escena se realiza en el dia 
+                    foreach (Scene auxScene in before.NightTime.Scenes)
+                    {
+                        foreach (Actor actor in auxScene.Actors)// recorre los actores del dia anterior
+                        { 
+                            foreach (Actor auxActor in scene.Actors)// recorre los actores de la escena actual, la que se desea agregar
+                            {
+                                if (actor.ID.Equals(auxActor.ID))
+                                {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (scene.Schedule == false) {// si la escena se realiza en la noche 
+                    foreach (Scene auxScene in before.DayTime.Scenes)
+                    {
+                        foreach (Actor actor in auxScene.Actors)
+                        {// recorre los actores del dia anterior 
+                            foreach (Actor auxActor in scene.Actors)
+                            {// recorre los actores de la escena actual, la que se desea agregar
+                                if (actor.ID.Equals(auxActor.ID))
+                                {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                } 
+            }
+            return true; // si no se encontraron coincidencias o si es el primer dia 
+        }
+
+        public static void accommodateScenesInDays(FilmingCalendar calendar, Movie movie, int positionScenario) {// resive el calendario que fue cruzado y se asignan las escenas del mismo a nuevos dias 
+            List<Day> newDays = createListWithDays(calendar);
+
+            foreach (Day day in newDays) {
+                foreach (Scene scene in calendar.Scenes)
+                {
+                    if (scene.Schedule == true) {
+                        int space = availableSpace(day.DayTime);// obtiene espacio disponible 
+                        if (scene.Pages <= space) {
+                            canAssignedToActor(day,newDays,scene);// validar si los actores no estan el dia anterior en la jornada contraria 
+                            day.DayTime.Scenes.Add(scene);
+                        }
+                    }
+                    if (scene.Schedule == false)
+                    {
+                        int space = availableSpace(day.NightTime);// obtiene espacio disponible 
+                        if (scene.Pages <= space)
+                        {
+                            canAssignedToActor(day, newDays, scene);// validar si los actores no estan el dia anterior en la jornada contraria 
+                            day.NightTime.Scenes.Add(scene);
+                        }
+
+                    }
+                }
+            }
+        } 
     }
 }
+
+// ver metodo donde se agregan los calendarios a la lista de calendarios de la scene, ahi mismo mandar a llamar a este metodo
