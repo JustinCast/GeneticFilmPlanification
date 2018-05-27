d259da640dfaf2441fc0f4719c8c9528b199a5b4
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-26 21:20:32 -0600

    Cambios en Branch&Bound

--- GeneticFilmPlanification/BranchAndBound.cs
+++ GeneticFilmPlanification/BranchAndBound.cs
@@ -10,6 +10,7 @@ namespace GeneticFilmPlanification
     class BranchAndBound
     {
         private List<FilmingCalendar> FilmingCalendars = new List<FilmingCalendar>();
+        private FilmingCalendar BSSF = 
         public BranchAndBound(List<Scenario> scenarios)
         {
             this.InitData(scenarios);
@@ -21,6 +22,9 @@ namespace GeneticFilmPlanification
         /// <param name="scenarios"></param>
         private void InitData(List<Scenario> scenarios)
         {
+            // la siguiente línea setea por defecto el primer calendario
+            // como mejor solución
+            this.BSSF = scenarios.ElementAt(0).FilmingCalendars.ElementAt(0);
             foreach(Scenario s in scenarios)
             {
                 this.FilmingCalendars.Add(s.FilmingCalendars.ElementAt(0))
@@ -31,7 +35,29 @@ namespace GeneticFilmPlanification
         {
             if (scenes.Count == 0)
                 return;
+            foreach(Scene s in scenes)
+            {
+                if (CombinationCost(scenes) > BSSF.Cost) // si el costo es mayor retorna la rec
+                    return;
+                MakeNewBSSF(scenes);
+                List<Scene> aux = scenes;
+                aux.Remove(s);
+                MakeCombination(aux);
+            }
 
+        }
+        /// <summary>
+        /// Creará una nueva mejor solución (calendario)
+        /// </summary>
+        /// <param name="scenes"></param>
+        private void MakeNewBSSF(List<Scene> scenes)
+        {
+
+        }
+
+        private int CombinationCost(List<Scene> scenes)
+        {
+            return 0;
         }
 
 
